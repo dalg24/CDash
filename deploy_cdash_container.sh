e9e050389a6e5f1ed8d0bc54250ad5dee6f55e6f
Damien L-G <qdi@ornl.gov>
2016-03-24 15:07:30 -0400

    update scripts that launch containers

--- deploy_cdash_container.sh
+++ deploy_cdash_container.sh
@@ -3,5 +3,7 @@
 docker run --detach \
   --publish 80:80 \
   --name cdash \
+  --env CDASH_DB_HOST=mysql:3306 \
+  --env CDASH_DB_PASSWORD=pass \
   --link mysql:mysql \
   cdash
--- deploy_mysql_container.sh
+++ deploy_mysql_container.sh
@@ -4,6 +4,6 @@ docker run --detach \
   --name mysql \
   --env MYSQL_ALLOW_EMPTY_PASSWORD=yes \
   --env MYSQL_USER=cdash \
-  --env MYSQL_PASSWORD="" \
+  --env MYSQL_PASSWORD=pass \
   --env MYSQL_DATABASE=cdash \
   mysql
