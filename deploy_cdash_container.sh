9c841324ebfcf7fa35fee61f1bce0eaba23256e8
Damien L-G <qdi@ornl.gov>
2016-03-24 16:06:02 -0400

    take passwords from env

--- deploy_cdash_container.sh
+++ deploy_cdash_container.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+if [ -z "${CDASH_DB_PASSWORD}" ]; then
+    echo >&2 "Nope..."
+    exit 1
+fi
+
 docker run --detach \
   --publish 80:80 \
   --name cdash \
-  --env CDASH_DB_HOST=mysql:3306 \
-  --env CDASH_DB_PASSWORD=pass \
   --link mysql:mysql \
+  --env CDASH_DB_HOST=mysql:3306 \
+  --env CDASH_DB_USER=cdash \
+  --env CDASH_DB_PASSWORD=${CDASH_DB_PASSWORD} \
+  --env CDASH_DB_NAME=cdash \
   cdash
--- deploy_mysql_container.sh
+++ deploy_mysql_container.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+if [ -z "${MYSQL_ROOT_PASSWORD}" -o -z "${CDASH_DB_PASSWORD}" ]; then
+    echo >&2 "Nope..."
+    exit 1
+fi
+
 docker run --detach \
   --name mysql \
+  --env MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
   --env MYSQL_ALLOW_EMPTY_PASSWORD=yes \
   --env MYSQL_USER=cdash \
-  --env MYSQL_PASSWORD=pass \
+  --env MYSQL_PASSWORD=${CDASH_DB_PASSWORD} \
   --env MYSQL_DATABASE=cdash \
   mysql
